6be6889a9519149486639eddf7f08f1b99866c1d
Rafael Days <130679216+Rafael-Days@users.noreply.github.com>
2024-03-22 14:17:45 -0300

    atividade

--- routes.ts
+++ routes.ts
@@ -4,6 +4,10 @@ import bookController from './src/books/book.controller'
 const routes = Router()
 routes.get('/health-check')
 routes.post('/books', bookController.create)
+routes.get('/books/:id', bookController.getById)
+routes.get('/books', bookController.getAll)
+routes.put('/books/:id', bookController.update)
+routes.delete('/books/:id', bookController.delete)
 
 export {
     routes
--- src/books/book.service.ts
+++ src/books/book.service.ts
@@ -7,4 +7,24 @@ export class BookService {
 
         return createdBook
     }
+    async getById(id: string) {
+        const getByIdbook = bookModel.findById(id)
+
+        return getByIdbook
+    }
+    async getAll() {
+        const getAllBook = bookModel.find()
+
+        return getAllBook
+    }
+    async update(id: string, newData: any) {
+        const updateBook = bookModel.findByIdAndUpdate(id, newData, { new: true })
+
+        return updateBook
+    }
+    async delete(id: string) {
+        const deleteBook = bookModel.findByIdAndDelete(id)
+
+        return deleteBook
+    }
 }
